b76c35a1f87fd9868b2d3bfd37201e129679c4c4
lawsann@gmail.com <lawsann@gmail.com@c0746c4b-df27-d1d7-0598-29480872bf8d>
2012-11-13 16:58:49 +0000

    Organizacao do codigo e retirada das funcoes do arquivo de template e passagem para os arquivos de bloco. Ainda sao necessarias melhorias no codigo, mas agora estah mais proximo do padrao desejado.
    Obs.: necessaria uma conferencia dos resultados, pois as funcoes sofreram alteracoes.

--- app/code/community/Maxima/BankSlip/Block/Bank.php
+++ app/code/community/Maxima/BankSlip/Block/Bank.php
@@ -25,17 +25,17 @@ class Maxima_BankSlip_Block_Bank extends Mage_Core_Block_Template
 			$dadosboleto["cedente"] 			= $this->getConfigData('company_official_name');
 			$dias_de_prazo_para_pagamento 		= $this->getConfigData('payment_term');
 			
-			$data_venc 							= date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006"; 
-			$valor_cobrado 						= $order->getGrandTotal(); // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+			$data_venc 							= date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));
+			$valor_cobrado 						= $order->getGrandTotal();
 			$valor_cobrado 						= str_replace(",", ".",$valor_cobrado);
 			$valor_boleto 						= number_format($valor_cobrado, 2, ',', '');
 
 			$dadosboleto["nosso_numero"] 		= $this->_formatSlipNumber($order->getId());
-			$dadosboleto["numero_documento"] 	= $order->getIncrementId();	// Num do pedido ou do documento
-			$dadosboleto["data_vencimento"] 	= $data_venc; // Data de Vencimento do Boleto - REGRA: Formato DD/MM/AAAA
-			$dadosboleto["data_documento"] 		= $this->_formatDate($order->getCreatedAt()); // Data de emissão do Boleto
-			$dadosboleto["data_processamento"] 	= $this->_formatDate($order->getCreatedAt()); // Data de processamento do boleto (opcional)
-			$dadosboleto["valor_boleto"] 		= $valor_boleto; 	// Valor do Boleto - REGRA: Com vírgula e sempre com duas casas depois da virgula
+			$dadosboleto["numero_documento"] 	= $order->getIncrementId();
+			$dadosboleto["data_vencimento"] 	= $data_venc;
+			$dadosboleto["data_documento"] 		= $this->_formatDate($order->getCreatedAt());
+			$dadosboleto["data_processamento"] 	= $this->_formatDate($order->getCreatedAt());
+			$dadosboleto["valor_boleto"] 		= $valor_boleto;
 
 			// DADOS DO SEU CLIENTE
 			$dadosboleto["sacado"] 				= $order->getCustomerName();
@@ -65,11 +65,10 @@ class Maxima_BankSlip_Block_Bank extends Mage_Core_Block_Template
 			$dadosboleto["especie"] 			= Mage::app()->getLocale()->currency(Mage::app()->getStore()->getCurrentCurrencyCode())->getSymbol() ;
 			$dadosboleto["especie_doc"] 		= "DM";
 			
-			
 			// DADOS DA SUA CONTA - DADOS DA CONTA
-			$dadosboleto["agencia"] 			= $this->getConfigData('agency'); // Num da agencia, sem digito
-			$dadosboleto["conta"] 				= $this->getConfigData('account'); 	// Num da conta, sem digito
-			$dadosboleto["conta_dv"] 			= $this->getConfigData('account_vd'); 	// Digito Verificador
+			$dadosboleto["agencia"] 			= $this->getConfigData('agency');
+			$dadosboleto["conta"] 				= $this->getConfigData('account');
+			$dadosboleto["conta_dv"] 			= $this->getConfigData('account_vd');
 		}
 		
 		return $dadosboleto;
@@ -94,7 +93,7 @@ class Maxima_BankSlip_Block_Bank extends Mage_Core_Block_Template
 	 * 
 	 */
 	
-	private function _formatSlipNumber($orderId)
+	protected function _formatSlipNumber($orderId)
 	{
 		while(strlen($orderId) < 5)
 		{
@@ -111,9 +110,255 @@ class Maxima_BankSlip_Block_Bank extends Mage_Core_Block_Template
 	 * 
 	 */
 	
-	private function _formatDate($date)
+	protected function _formatDate($date)
 	{
 		return (substr($date, 8, 2) . "/" . substr($date, 5, 2) . "/" . substr($date, 0, 4));
+	}
+	
+	/**
+	 * 
+	 * Formata numeros utilizados no boleto, preenchendo com o caractere
+	 * determinado por $insertValue, deixando o numero com o tamanho determinado
+	 * pelo segund parametro
+	 * 
+	 */
+	protected function _formatNumber($number, $size, $insertValue, $type = "default")
+	{
+		// para cada caso, retira a virgula e insere
+		// o valor na posicao respectiva ao formato
+		
+		// padrao
+		if ($type == "default")
+		{
+			$number = str_replace(",","",$number);
+			
+			while(strlen($number) < $size)
+			{
+				$number = $insertValue . $number;
+			}
+		}
+		
+		// valor
+		if ($type == "value")
+		{
+			$number = str_replace(",","",$number);
+			
+			while(strlen($number) < $size)
+			{
+				$number = $insertValue . $number;
+			}
+		}
+		
+		// convenio
+		if ($type == "agreement")
+		{
+			while(strlen($number)<$size)
+			{
+				$number = $number . $insertValue;
+			}
+		}
+		
+		return $number;
+	}
+	
+	/**
+	 * 
+	 * Gera codigo de barras em HTML, utilizando imagens do modulo
+	 * 
+	 */
+	protected function _generateBarCode($value)
+	{
+		$thin = 1 ;
+		$large = 3 ;
+		$height = 50 ;
+		
+		$barCodes = array();
+		$barCodes[0] = "00110" ;
+		$barCodes[1] = "10001" ;
+		$barCodes[2] = "01001" ;
+		$barCodes[3] = "11000" ;
+		$barCodes[4] = "00101" ;
+		$barCodes[5] = "10100" ;
+		$barCodes[6] = "01100" ;
+		$barCodes[7] = "00011" ;
+		$barCodes[8] = "10010" ;
+		$barCodes[9] = "01010" ;
+		
+		
+		for($f1 = 9; $f1 >= 0; $f1--)
+		{
+			for($f2 = 9; $f2 >= 0; $f2--)
+			{
+				$f = ($f1 * 10) + $f2;
+				$text = "";
+				
+				for($i = 1; $i < 6; $i++)
+				{
+					$text .=  substr($barCodes[$f1], ($i - 1), 1) . substr($barCodes[$f2], ($i - 1), 1);
+				}
+				
+				$barCodes[$f] = $text;
+			}
+		}
+
+
+		// imagens
+		// guarda inicial
+		$imagesText  = "<img src='" . Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB) . 
+					   "skin/frontend/default/default/images/maxima/bankslip/p.png' width='" . 
+					   $thin . "' height='" . $height . "' border='0'>";
+					   
+		$imagesText .= "<img src='" . Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB) . 
+					   "skin/frontend/default/default/images/maxima/bankslip/b.png' width='" . 
+					   $thin . "' height='" . $height . "' border='0'>";
+		
+		
+		$imagesText .= "<img src='" . Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB) .
+					   "skin/frontend/default/default/images/maxima/bankslip/p.png' width='" . 
+					   $thin . "' height='" . $height . "' border='0'>";
+					   
+		$imagesText .= "<img src='" . Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB) . 
+					   "skin/frontend/default/default/images/maxima/bankslip/b.png' width=" . 
+					   $thin . "' height='" . $height . "' border='0'>";
+		
+		$text = $value;
+		if((strlen($text) % 2) <> 0)
+		{
+			$text = "0" . $text;
+		}
+
+		// codigo de barra dos dados
+		while(strlen($text) > 0)
+		{
+			$i = round($this->_leftSubstring($text,2));
+			$text = $this->_rightSubstring($text,strlen($text)-2);
+			$f = $barCodes[$i];
+			for($i=1;$i<11;$i+=2)
+			{
+				if (substr($f,($i-1),1) == "0")
+				{
+					$f1 = $thin ;
+				}
+				else
+				{
+					$f1 = $large ;
+				}
+				
+				$imagesText .= "<img src='" . Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB) . 
+							   "skin/frontend/default/default/images/maxima/bankslip/p.png' width='" . 
+							   $f1 . "' height='" . $height . "' border='0'>";
+				
+				
+				if (substr($f,$i,1) == "0")
+				{
+					$f2 = $thin ;
+				}
+				else
+				{
+					$f2 = $large ;
+				}
+				
+				$imagesText .= "<img src='" . Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB) . 
+							   "skin/frontend/default/default/images/maxima/bankslip/b.png' width='" . 
+							   $f2 . "' height='" . $height . "' border='0'>";
+			}
+		}
+
+		// guarda final
+		$imagesText .= "<img src='" . Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB) . 
+							   "skin/frontend/default/default/images/maxima/bankslip/p.png' width='" . 
+							   $large . "' height='" . $height . "' border='0'>";
+		
+		$imagesText .= "<img src='" . Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB) . 
+							   "skin/frontend/default/default/images/maxima/bankslip/b.png' width='" . 
+							   $thin . "' height='" . $height . "' border='0'>";
+		
+		$imagesText .= "<img src='" . Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB) . 
+							   "skin/frontend/default/default/images/maxima/bankslip/p.png' width='1' " . 
+							   "height='" . $height . "' border='0'>";
+		
+		return $imagesText;
+	}
+	
+	/**
+	 * 
+	 * Gera a linha digitavel do boleto
+	 * 
+	 */
+	protected function _generateBankNumber($number)
+	{
+		$part1 = substr($number, 0, 3);
+		$part2 = $this->_module_11($number);
+		
+		return $part1 . "-" . $part2;
+	}
+	
+	/**
+	 * 
+	 * Retorna parte da string, iniciando da esquerda
+	 * 
+	 */
+	protected function _leftSubstring($str, $size)
+	{
+		return substr($str, 0, $size);
+	}
+	
+	/**
+	 * 
+	 * Retorna parte da string, iniciando da direita
+	 * 
+	 */
+	protected function _rightSubstring($str, $size)
+	{
+		return substr($str, strlen($str) - $size, $size);
+	}
+	
+	/**
+	 * 
+	 * Retorna o fator de vencimento
+	 * 
+	 */
+	protected function _maturityFactor($date)
+	{
+		$date = explode("/", $date);
+		$year = $date[2];
+		$month = $date[1];
+		$day = $date[0];
+		return(abs(($this->_dateToDays("1997","10","07")) - ($this->_dateToDays($year, $month, $day))));
+	}
+	
+	/**
+	 * 
+	 * Contabiliza o numero de dias, dada uma data
+	 * 
+	 */
+	protected function _dateToDays($year, $month, $day)
+	{
+		$century = substr($year, 0, 2);
+		$year = substr($year, 2, 2);
+		
+		if ($month > 2)
+		{
+			$month -= 3;
+		}
+		else
+		{
+			$month += 9;
+			if ($year)
+			{
+				$year--;
+			}
+			else
+			{
+				$year = 99;
+				$century --;
+			}
+		}
+
+		return (floor((146097 * $century) / 4) +
+				floor((1461 * $year) / 4) +
+				floor((153 * $month + 2) / 5) +
+				$day + 1721119);
 	}
 }
  
--- app/code/community/Maxima/BankSlip/Block/Itau.php
+++ app/code/community/Maxima/BankSlip/Block/Itau.php
@@ -14,9 +14,166 @@ class Maxima_BankSlip_Block_Itau extends Maxima_BankSlip_Block_Bank
 			$dadosboleto = parent::getSlipData();
 
 			// DADOS PERSONALIZADOS - ITAU
-			$dadosboleto["carteira"] 			= $this->getConfigData('book'); // Código da Carteira: pode ser 175, 174, 104, 109, 178, ou 157
+			$dadosboleto["carteira"] = $this->getConfigData('book');
 		}
 		
 		return $dadosboleto;
+	}
+	
+	
+	/**
+	 * 
+	 * Gera a linha digitavel do boleto
+	 * 
+	 */
+	protected function _generateBarCodeNumber($line)
+	{
+		// campo 1
+        $bank		= substr($line, 0, 3);
+        $currency	= substr($line, 3, 1);
+        $ccc		= substr($line, 19, 3);
+		$ddnnum		= substr($line, 22, 2);
+		$vDigit1	= $this->_module_10($bank . $currency . $ccc . $ddnnum);
+		
+		// campo 2
+		$resnnum	= substr($line, 24, 6);
+		$dac1		= substr($line, 30, 1);
+		$dddag		= substr($line, 31, 3);
+		$vDigit2	= $this->_module_10($resnnum.$dac1.$dddag);
+		
+		// campo 3
+		$resag		= substr($line, 34, 1);
+		$contadac	= substr($line, 35, 6);
+		$zeros		= substr($line, 41, 3);
+		$vDigit3	= $this->_module_10($resag.$contadac.$zeros);
+		
+		// campo 4
+		$vDigit4	= substr($line, 4, 1);
+		
+		// campo 5
+        $factor		= substr($line, 5, 4);
+        $value		= substr($line, 9, 10);
+		
+        $field1 	= substr($bank.$currency.$ccc.$ddnnum.$vDigit1,0,5) . '.' . substr($bank.$currency.$ccc.$ddnnum.$vDigit1,5,5);
+        $field2 	= substr($resnnum.$dac1.$dddag.$vDigit2,0,5) . '.' . substr($resnnum.$dac1.$dddag.$vDigit2,5,6);
+        $field3 	= substr($resag.$contadac.$zeros.$vDigit3,0,5) . '.' . substr($resag.$contadac.$zeros.$vDigit3,5,6);
+        $field4 	= $vDigit4;
+        $field5 	= $factor . $value;
+		
+        return "$field1 $field2 $field3 $field4 $field5";
+	}
+	
+	/**
+	 * 
+	 * Digito verificador do numero do codigo de barras
+	 * 
+	 */
+	protected function _generateBarCodeVerificationDigit($number)
+	{
+		$rest = $this->_module_11($number, 9, 1);
+		$digit = 11 - $rest;
+		
+		if ($digit == 0 || $digit == 1 || $digit == 10  || $digit == 11)
+		{
+			$verificationDigit = 1;
+		}
+		else
+		{
+			$verificationDigit = $digit;
+		}
+		
+		return $verificationDigit;
+	}
+	
+	/**
+	 * 
+	 * Utilizada para geracao do digito verificador
+	 * 
+	 */
+	protected function _module_10($num)
+	{
+		$total = 0;
+        $factor = 2;
+		
+		$partial = array();
+		$numbers = array();
+		
+        for ($i = strlen($num); $i > 0; $i--)
+		{
+			$numbers[$i] = substr($num, $i - 1, 1);
+            $temp = $numbers[$i] * $factor; 
+            $temp0 = 0;
+			
+            foreach(preg_split('//', $temp, -1, PREG_SPLIT_NO_EMPTY) as $k => $v)
+			{
+				$temp0 += $v;
+			}
+            
+			$partial[$i] = $temp0;
+            $total += $partial[$i];
+			
+            if ($factor == 2)
+			{
+                $factor = 1;
+            }
+			else
+			{
+                $factor = 2;
+            }
+        }
+		
+        $rest = $total % 10;
+        $digit = 10 - $rest;
+		
+        if ($rest == 0)
+		{
+            $digit = 0;
+        }
+		
+        return $digit;
+	}
+	
+	/**
+	 * 
+	 * Utilizada para geracao do digito verificador
+	 * 
+	 */
+	protected function _module_11($num, $base = 9, $r = 0)
+	{
+		$sum = 0;
+		$factor = 2;
+		$numbers = array();
+		$partial = array();
+
+		for ($i = strlen($num); $i > 0; $i--)
+		{
+			$numbers[$i] = substr($num, $i-1, 1);
+			$partial[$i] = $numbers[$i] * $factor;
+			$sum += $partial[$i];
+			
+			if ($factor == $base)
+			{
+				$factor = 1;
+			}
+			
+			$factor++;
+		}
+
+		if ($r == 0)
+		{
+			$sum *= 10;
+			$digit = $sum % 11;
+			if ($digit == 10)
+			{
+				$digit = 0;
+			}
+			
+			return $digit;
+		}
+		else if($r == 1)
+		{
+			$rest = $sum % 11;
+			return $rest;
+		}
 	}
 }
